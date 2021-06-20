5a03ef1767eaf76af1077cc85d69739e4e4de114
Henri Tukiainen <henri@momocode.com>
2021-06-21 02:02:40 +0300

    Add logo to header

--- web/app/themes/urban-tech-helsinki/resources/views/partials/header.blade.php
+++ web/app/themes/urban-tech-helsinki/resources/views/partials/header.blade.php
@@ -1,15 +1,8 @@
 <div class="site-header">
   <a class="site-header__logo" href="{{ home_url('/') }}" rel="home" aria-label="{{ __('Go to frontpage', 'hds') }}">
-    @if (mb_substr(get_locale(), 0, 2) === 'sv')
-      @svg('images/logo/helsinki-sv.svg', '', ['width' => 67, 'height' => 31])
-    @else
-      @svg('images/logo/helsinki-fi.svg', '', ['width' => 67, 'height' => 31])
-    @endif
+    <img src="{{ \Roots\Asset('images/logo.png') }}" alt="{{ $site_name }}" />
+    @svg('images/logo.png', '', ['width' => 67, 'height' => 31])
   </a>
-
-  <div class="site-header__name">
-    <h5>{{ $site_name }}</h5>
-  </div>
 
   @if ($search_enabled)
     <div class="site-header__search">
