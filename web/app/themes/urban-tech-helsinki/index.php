3d372caab39308b756cfa3e3fd75904faa597cd0
Henri Tukiainen <henri@momocode.com>
2021-09-12 00:44:58 +0300

    Add Matomo tracking with Complianz support, pending site id

--- web/app/themes/urban-tech-helsinki/index.php
+++ web/app/themes/urban-tech-helsinki/index.php
@@ -17,5 +17,21 @@
 
     <?php do_action('get_footer'); ?>
     <?php wp_footer(); ?>
+
+    <!-- Matomo -->
+    <script type="text/plain" class="cmplz-stats">
+      var _paq = window._paq = window._paq || [];
+      /* tracker methods like "setCustomDimension" should be called before "trackPageView" */
+      _paq.push(['trackPageView']);
+      _paq.push(['enableLinkTracking']);
+      (function() {
+        var u="//webanalytics.digiaiiris.com/js/";
+        _paq.push(['setTrackerUrl', u+'tracker.php']);
+        _paq.push(['setSiteId', '0']);
+        var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
+        g.type='text/javascript'; g.async=true; g.src=u+'piwik.min.js'; s.parentNode.insertBefore(g,s);
+      })();
+    </script>
+    <!-- End Matomo Code -->
   </body>
 </html>
