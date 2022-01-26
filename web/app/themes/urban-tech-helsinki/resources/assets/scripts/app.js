6497b83a89d641bc4fad66cfc83a685f99efe2e0
Henri Tukiainen <henri@momocode.com>
2022-01-26 19:49:53 +0200

    Support submenu in top navigation

--- web/app/themes/urban-tech-helsinki/resources/assets/scripts/app.js
+++ web/app/themes/urban-tech-helsinki/resources/assets/scripts/app.js
@@ -5,6 +5,22 @@ window.addEventListener('resize', debounce(setAppHeight, 150));
 
 addToggleListener(document.querySelector('.site-hamburger-button'))
 
+const submenuTriggers = document.querySelectorAll('.site-navigation__submenu-trigger');
+for (let i = 0; i < submenuTriggers.length; i++) {
+  const trigger = submenuTriggers[i];
+  trigger.addEventListener('click', (e) => {
+    e.preventDefault();
+    const item = e.target.closest('[aria-haspopup]');
+    toggle(item);
+  })
+}
+
+if (matchMedia('(min-width: 1024px)').matches) {
+  if (document.querySelector('.site-navigation')) {
+    menu(document.querySelector('.site-navigation'));
+  }
+}
+
 function setAppHeight () {
   const doc = document.documentElement
   doc.style.setProperty('--app-height', `${window.innerHeight}px`)
@@ -29,6 +45,82 @@ function toggleAriaExpanded (el) {
       if (el) {
         el.classList[isExpanded ? 'remove' : 'add']('is-expanded')
       }
+    }))
+  }
+}
+
+function toggle(el) {
+  const isExpanded = el.getAttribute('aria-expanded') === 'true';
+  if (isExpanded) {
+    close(el);
+  } else {
+    open(el);
+  }
+
+  const controls = el.getAttribute('aria-controls');
+  if (controls) {
+    controls.split(' ').forEach((selector) => {
+      const el  = document.getElementById(selector);
+      if (isExpanded) {
+        el.classList.remove('is-active');
+      }
+      else {
+        el.classList.add('is-active');
+      }
+    });
+  }
+
+  function open(el) {
+    el.setAttribute('aria-expanded', true);
+  }
+
+  function close(el) {
+    el.setAttribute('aria-expanded', false);
+  }
+}
+
+function openItem (itemEl) {
+  if (!itemEl) return
+  const controlEl = itemEl.querySelector('[aria-haspopup="true"]')
+  const controlsEl = controlEl && controlEl.getAttribute('aria-controls')
+    ? document.getElementById(controlEl.getAttribute('aria-controls'))
+    : undefined
+  if (controlEl) {
+    controlEl.setAttribute('aria-expanded', true);
+  }
+  if (controlsEl) {
+    controlsEl.classList.add('is-active')
+  }
+}
+
+function closeItem (itemEl) {
+  if (!itemEl) return
+  const controlEl = itemEl.querySelector('[aria-haspopup="true"]')
+  const controlsEl = controlEl && controlEl.getAttribute('aria-controls')
+    ? document.getElementById(controlEl.getAttribute('aria-controls'))
+    : undefined
+  if (controlEl) {
+    controlEl.setAttribute('aria-expanded', false);
+  }
+  if (controlsEl) {
+    controlsEl.classList.remove('is-active')
+  }
+}
+
+export function menu(menu) {
+  const itemsWithSubmenu = menu.querySelectorAll('.has-children')
+
+  for (let i = 0; i < itemsWithSubmenu.length; i++) {
+    const itemWithSubmenu = itemsWithSubmenu[i]
+
+    itemWithSubmenu.addEventListener('mouseover', () => requestAnimationFrame(() => {
+      // Close other submenus
+      Array.from(itemsWithSubmenu).forEach(closeItem)
+      openItem(itemWithSubmenu)
+    }))
+
+    itemWithSubmenu.addEventListener('mouseout', () => requestAnimationFrame(() => {
+      closeItem(itemWithSubmenu)
     }))
   }
 }
--- web/app/themes/urban-tech-helsinki/resources/views/partials/menu-item.blade.php
+++ web/app/themes/urban-tech-helsinki/resources/views/partials/menu-item.blade.php
@@ -10,13 +10,44 @@
   aria-label="{{ esc_attr($item->label) }}"
   role="none"
 >
-  <a
-    href="{{ $item->url }}"
-    target="{{ $item->target ?? '' }}"
-    title="{{ $item->title ?? '' }}"
-    class="{{ $name }}__link {{ ($item->active || $item->activeAncestor) ? 'is-active': '' }}"
-    role="menuitem"
+  <div class="{{ $name }}__link-container">
+    <a
+      href="{{ $item->url }}"
+      target="{{ $item->target ?? '' }}"
+      title="{{ $item->title ?? '' }}"
+      class="{{ $name }}__link {{ ($item->active || $item->activeAncestor) ? 'is-active': '' }}"
+      role="menuitem"
+    >
+      <span>
+        {!! esc_html($item->label) !!}
+      </span>
+    </a>
+    @if ($item->children)
+    <button
+      class="{{ $name }}__submenu-trigger"
+      aria-label="{{ sprintf(__('%s submenu', 'hds'), $label ?? $item->label) }}"
+      aria-controls="submenu-{{ $item->slug }}"
+      role="button"
+      type="button"
+      aria-haspopup="true"
+      aria-expanded="false"
+    >
+      <span
+        class="{{ $name }}__submenu-icon"
+        aria-hidden="true"
+      >
+      </span>
+    </button>
+    @endif
+  </div>
+  @if ($item->children)
+  <ul
+    id="submenu-{{ $item->slug }}" class="{{ $name }}__submenu"
+    aria-label="{!! esc_attr($item->label) !!}"
   >
-    {!! esc_html($item->label) !!}
-  </a>
+    @foreach ($item->children as $child)
+      @include('partials.menu-item', ['item' => $child, 'name' => $name, 'level' => $level + 1])
+    @endforeach
+  </ul>
+  @endif
 </li>
